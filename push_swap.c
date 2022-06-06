b5a4421bc65a35d303b90e62a2c7cd828a2e0211
olin_tp_ar <myemail@archi.com>
2022-06-06 15:56:56 +0200

    error handling numerics

--- push_swap.c
+++ push_swap.c
@@ -6,8 +6,72 @@
 /*   By: kaheinz <kaheinz@student.42wolfsburg.de>   +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/06/05 12:33:24 by kaheinz           #+#    #+#             */
-/*   Updated: 2022/06/05 12:33:25 by kaheinz          ###   ########.fr       */
+/*   Updated: 2022/06/06 15:56:06 by kaheinz          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#include "push_swap.h"
 
+int	checker(long int nbr)
+{
+	if (nbr > MAX_INT)
+	{
+		printf("Error\n");
+		exit(EXIT_FAILURE);
+	}
+	else if (nbr < MIN_INT)
+	{
+		printf("Error\n");
+		exit(EXIT_FAILURE);
+	}
+	else
+	{
+		printf("Your integer is: %i", nbr);
+		return(0);
+	}
+
+}
+
+
+int	numeric_checker(char *arg)
+{
+	int	c;
+//	int	sign;
+
+	c = 0;
+//	sign = 1;
+	while (arg[c])
+	{
+		if (arg[c] > '9' || arg[c] < '0')
+		//	printf("Iteration %c\n", arg[c]);
+		{
+			printf("Error\n");
+			exit(EXIT_FAILURE);
+		}
+		else
+//			printf("Numeric value");
+			c++;
+	}
+//	printf("all numerics");
+	return (0);
+}
+
+int	main(int argc, char **argv)
+{
+	int	i;
+//	long int	nbr;
+
+	i = 1;
+	if (argc == 1)
+		return(0);
+	while(argv[i])
+	{
+	//	nbr = atoi(argv[1]);
+	//	checker(atol(argv[i]));	
+		if (numeric_checker(argv[i]))
+			checker(atol(argv[i]));	
+//		printf("Iteration %s\n", argv[i]);
+		i++;
+	}
+	return (0);
+}
---
+++ push_swap.h
@@ -0,0 +1,25 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   push_swap.h                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: kaheinz <kaheinz@student.42wolfsburg.de>   +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/06/05 16:12:08 by kaheinz           #+#    #+#             */
+/*   Updated: 2022/06/06 15:27:49 by kaheinz          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef PUSH_SWAP_H
+# define PUSH_SWAP_H
+# define MAX_INT 2147483647
+# define MIN_INT -2147483648
+
+#include <stdio.h>
+#include <stdlib.h>
+
+//int	checker(long int nbr);
+int	numeric_checker(char *arg);
+
+
+#endif
