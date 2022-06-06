3f3fe96d0014518d6193d97bd1491952fea622de
olin_tp_ar <myemail@archi.com>
2022-06-06 18:48:47 +0200

    error handling

--- push_swap.c
+++ push_swap.c
@@ -6,11 +6,34 @@
 /*   By: kaheinz <kaheinz@student.42wolfsburg.de>   +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/06/05 12:33:24 by kaheinz           #+#    #+#             */
-/*   Updated: 2022/06/06 16:57:12 by kaheinz          ###   ########.fr       */
+/*   Updated: 2022/06/06 18:48:07 by kaheinz          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "push_swap.h"
+
+int	duplicates_checker(int argc, char **argv)
+{
+	int	i;
+	int	j;
+
+	i = 1;
+	while (i < argc)
+	{
+		j = i + 1;
+		while (j < argc)
+		{
+			if ((atoi(argv[i]) == atoi(argv[j])))
+			{
+				printf("Duplicate found!\n");
+				exit (EXIT_FAILURE);
+			}
+			j++;
+		}
+		i++;
+	}
+	return (1);
+}
 
 int	checker(long int nbr)
 {
@@ -25,13 +48,8 @@ int	checker(long int nbr)
 		exit(EXIT_FAILURE);
 	}
 	else
-	{
-		printf("Your integer is: %i", nbr);
-		return(0);
-	}
-
+		return (1);
 }
-
 
 int	numeric_checker(char *arg)
 {
@@ -43,35 +61,29 @@ int	numeric_checker(char *arg)
 		if (arg[c] == '-' || arg[c] == '+')
 			c++;
 		if (arg[c] > '9' || arg[c] < '0')
-		//	printf("Iteration %c\n", arg[c]);
 		{
 			printf("Error non numeric\n");
 			exit(EXIT_FAILURE);
 		}
 		else
-//			printf("Numeric value");
 			c++;
 	}
-//	printf("all numerics");
-	return (0);
+	return (1);
 }
 
 int	main(int argc, char **argv)
 {
 	int	i;
-//	long int	nbr;
 
 	i = 1;
 	if (argc == 1)
-		return(0);
-	while(argv[i])
+		return (0);
+	while (argv[i])
 	{
-	//	nbr = atoi(argv[1]);
-	//	checker(atol(argv[i]));	
-		if (numeric_checker(argv[i]) == 0)
-			checker(atol(argv[i]));	
-//		printf("Iteration %s\n", argv[i]);
-		i++;
+		if (numeric_checker(argv[i]) && checker(atol(argv[i])))
+			i++;
 	}
+	if (duplicates_checker(argc, argv))
+		printf("numeric, min/max pass, no dups");
 	return (0);
 }
--- push_swap.h
+++ push_swap.h
@@ -6,7 +6,7 @@
 /*   By: kaheinz <kaheinz@student.42wolfsburg.de>   +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/06/05 16:12:08 by kaheinz           #+#    #+#             */
-/*   Updated: 2022/06/06 15:58:04 by kaheinz          ###   ########.fr       */
+/*   Updated: 2022/06/06 18:18:17 by kaheinz          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,11 +15,11 @@
 # define MAX_INT 2147483647
 # define MIN_INT -2147483648
 
-#include <stdio.h>
-#include <stdlib.h>
+# include <stdio.h>
+# include <stdlib.h>
 
 int	checker(long int nbr);
 int	numeric_checker(char *arg);
-
+int	duplicates_checker(int argc, char **argv);
 
 #endif
